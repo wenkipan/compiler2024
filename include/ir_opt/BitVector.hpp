3fcdba9160501f938065a7340326f3556db292c8
retcarizy <2422432892@qq.com>
2024-05-22 20:13:19 +0800

    bit_vector and LivenessAnalysis

---
+++ include/ir_opt/BitVector.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+
+class bit_vector
+{
+    std::vector<unsigned int> bits;
+    int num;
+
+public:
+    bit_vector();
+    bit_vector(int n);
+    bool at(int id);
+    void set(int id, bool x);
+    bit_vector &operator=(const bit_vector &bv);
+    bit_vector operator&(bit_vector bv);
+    bit_vector operator|(bit_vector bv);
+    bit_vector operator^(bit_vector bv);
+    bit_vector operator~();
+    bool operator==(bit_vector bv);
+    bool operator!=(bit_vector bv);
+};
---
+++ src/ir_opt/BitVector.cpp
@@ -0,0 +1,91 @@
+#include <ir_opt/BitVector.hpp>
+
+bit_vector::bit_vector()
+{
+    num = 0;
+}
+
+bit_vector::bit_vector(int n)
+{
+    num = n;
+    bits.resize((num - 1) / 32 + 1);
+}
+
+bool bit_vector::at(int id)
+{
+    if (id < 0 || id >= num)
+        assert(0);
+    return bits[id >> 5] >> (id & 31) & 1;
+}
+
+void bit_vector::set(int id, bool x)
+{
+    if (id < 0 || id >= num)
+        assert(0);
+    bits[id >> 5] |= (1 << (id & 31));
+    if (!x)
+        bits[id >> 5] ^= (1 << (id & 31));
+}
+
+bit_vector &bit_vector::operator=(const bit_vector &bv)
+{
+    this->bits = bv.bits;
+    this->num = bv.num;
+    return *this;
+}
+
+bit_vector bit_vector::operator&(bit_vector bv)
+{
+    if (num != bv.num)
+        assert(0);
+    int len = bits.size();
+    for (int i = 0; i < len; i++)
+        bv.bits[i] &= bits[i];
+    return bv;
+}
+
+bit_vector bit_vector::operator|(bit_vector bv)
+{
+    if (num != bv.num)
+        assert(0);
+    int len = bits.size();
+    for (int i = 0; i < len; i++)
+        bv.bits[i] |= bits[i];
+    return bv;
+}
+
+bit_vector bit_vector::operator^(bit_vector bv)
+{
+    if (num != bv.num)
+        assert(0);
+    int len = bits.size();
+    for (int i = 0; i < len; i++)
+        bv.bits[i] ^= bits[i];
+    return bv;
+}
+
+bit_vector bit_vector::operator~()
+{
+    bit_vector ret(num);
+    int len = bits.size();
+    for (int i = 0; i < len; i++)
+        ret.bits[i] = ~bits[i];
+    ret.bits[len - 1] &= ((1 << (num & 31)) - 1);
+    return ret;
+}
+
+bool bit_vector::operator==(bit_vector bv)
+{
+    if (num != bv.num)
+        return false;
+    int len = bits.size();
+    for (int i = 0; i < len; i++)
+        if (bits[i] != bv.bits[i])
+            return false;
+    return true;
+}
+
+bool bit_vector::operator!=(bit_vector bv)
+{
+    return !(*this == bv);
+}
