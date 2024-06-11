866d80d41e1a1b118e56102bad9d4ef701888f24
retcarizy <2422432892@qq.com>
2024-06-11 15:11:19 +0800

    mem2reg allow ptr

--- src/ir_opt/Mem2Reg.cpp
+++ src/ir_opt/Mem2Reg.cpp
@@ -58,8 +58,8 @@ bool Mem2Reg::rewriteSingleStoreAlloca(Alloca *alloc)
 
 bool Mem2Reg::isPromote(Alloca *alloc)
 {
-    if (dynamic_cast<Ptr *>(alloc->get_type())->get_btype()->get_type() != TypeEnum::Ptr && dynamic_cast<Ptr *>(alloc->get_type())->get_btype()->get_type() != TypeEnum::F32 && dynamic_cast<Ptr *>(alloc->get_type())->get_btype()->get_type() != TypeEnum::I32)
-        return false;
+    // if (dynamic_cast<Ptr *>(alloc->get_type())->get_btype()->get_type() != TypeEnum::F32 and dynamic_cast<Ptr *>(alloc->get_type())->get_btype()->get_type() != TypeEnum::I32)
+    //   return false;
     for (Edge *edge : *(alloc->get_user_list()))
     {
         User *user = (User *)(edge->get_user());
