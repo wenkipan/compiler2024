dbca6e305c8bf0178a9a7f81f9da0e3f42cbb03c
wenkipan <1024381676@qq.com>
2024-06-20 14:12:55 +0800

    imul to reg

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
     // freopen("in.txt", "r", stdin);
-    //  freopen("out.txt", "w", stdout);
+    // freopen("out.txt", "w", stdout);
     char *in_file = NULL, *out_file = NULL;
     std::string Infile, Outfile;
     bool is_opt = false;
@@ -75,6 +75,7 @@ int main(int argc, char *argv[])
     manager->run<immeFloatToLoad>();
     manager->run<LargeToGlobal>();
     manager->printModule();
+    fflush(stdout);
     //  2lir
     //  module->lowerIR();
     //  module->print();
